6c0514edfbc1be3658463ecef562ab511b59cd5c
Ceyhun1105 <7rz0svk@code.edu.az>
2022-12-30 13:16:44 +0400

    Eterna

---
+++ CCTV/CCTV/Migrations/20221229061901_CreateServicesTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CCTV.Migrations
+{
+    public partial class CreateServicesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CCTV/CCTV/Models/Price.cs
@@ -0,0 +1,19 @@
+﻿namespace CCTV.Models
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public string Plan { get; set; }
+        public int Money { get; set; }
+        public string TimeRange { get; set; }
+        public string Features { get; set; }
+        public string OrderUrl { get; set; }
+    }
+}
+
+
+
+
+                            
+
+
